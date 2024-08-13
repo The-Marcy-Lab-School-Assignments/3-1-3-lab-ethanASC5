d8d2331b2ea6711df84aa0ff1a75954180d2b846
Marcy <marcy@Marcys-MBP-4.home>
2024-08-13 02:36:01 -0400

    finished

--- src/render-functions.js
+++ src/render-functions.js
@@ -21,10 +21,40 @@ export const renderBookList = (bookListEl, books) => {
 }
 
 export const renderAuthorInfo = (authorInfoEl, author) => {
-}
+	authorInfoEl.innerHTML = "";
+	const h2 = document.createElement("h2");
+	const img = document.createElement("img");
+	const p = document.createElement("p");
+	const p2 = document.createElement("p");
+	const a = document.createElement("a");
+
+	h2.textContent = author.name;
+	img.src = author.pictureUrl;
+	img.alt = `A picture of ${author.name}`;
+	p.textContent = `Born: ${author.birthDate}`;
+	p2.textContent = author.bio;
+	a.href = author.wikipediaUrl;
+	a.textContent = `Wikipedia Link`;
+
+	authorInfoEl.append(h2, img, p, p2, a);
+};
 
 export const renderNewUserForm = (newUserFormEl) => {
-}
+	newUserFormEl.innerHTML = `
+	<label for="username">Username</label>
+	<input id="username" name="username">
+	<label for="is-cool">Is this user cool?</label>
+	<input type="checkbox" id="is-cool" name="isCool">
+	<label for="favorite-language">Favorite Language</label>
+	<select name="favoriteLanguage" id="favorite-language">
+		<option value="None">None</option>
+		<option value="JavaScript">JavaScript</option>
+		<option value="Python">Python</option>
+		<option value="Ruby">Ruby</option>
+	</select>
+	<button>Create User</button>`;
+};
+
 
 export const renderNewUser = (newUserEl, newUser) => {
 }
